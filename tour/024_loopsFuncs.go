1e003d1850815d9355ce242c2df5a61082fb458b
Jatindera Walia <jatindera_walia@hotmail.com>
2019-04-07 19:49:11 -0500

    sqrt: Newton's method

--- tour/024_loopsFuncs.go
+++ tour/024_loopsFuncs.go
@@ -3,10 +3,31 @@
 package main
 
 import "fmt"
+import "math"
 
-func Sqrt(x float64) float64 {
+func sqrt(x float64) float64 {
+	z := 1.0
+	// z = x, x/2 etc.
+	//z = x / 2
+	zi := z
+	eps := 0.0000000000001
+	delta := 0.0
+	//for i := 0; i < 10; i++ {
+	for {
+		zi = z
+		z -= (z*z - x) / (2 * z)
+
+		delta = math.Abs(z - zi)
+		//fmt.Println(z)
+		fmt.Println("[zi:", zi, "][z", z, "][delta :", delta, "]")
+		if delta < eps {
+			break
+		}
+		//fmt.Println(z)
+	}
+	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(sqrt(256))
 }
