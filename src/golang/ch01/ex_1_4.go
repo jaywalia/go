b5d3b87ac6946f32ae5f49c16c751348e58e8c66
jay walia <jatinderasingh@gmail.com>
2021-09-12 18:43:17 +0000

    go book

---
+++ src/golang/ch01/ex_1_4.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	// read files
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup files 2: %v\n", err)
+			continue
+		}
+		// count lines
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+			// TODO: keep filenames with line count
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%v\n", n, line)
+		}
+	}
+}
